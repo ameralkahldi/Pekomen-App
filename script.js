97120421ccbdafa7eaeb8061bfe631be30b83f68
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-07-08 14:22:35 +0200

    amer+08.07.2025

---
+++ script.js
@@ -0,0 +1,50 @@
+const url = "https://pokeapi.co/api/v2/pokemon";
+let pokemon_list = document.getElementById("pokemons");
+let loadMoreBtn = document.getElementById('loadmore');
+let searchInput = document.getElementById("input_search");
+const mainContain = document.getElementById("main");
+const loading = document.getElementById("loading");
+const detailPokemonId = document.getElementById("detailPokemonId");
+const pokemonAlreadyLoaded = {};
+
+
+function init(){
+    renderPokemon()
+
+}
+
+
+async function renderPokemon() {
+  const response = await fetch(url);
+  const data = await response.json();
+  data.results.forEach(detailPokemon);
+}
+
+
+async function detailPokemon(pokemon) {
+  let detailpokemon;
+  if(pokemonAlreadyLoaded[pokemon.name]){
+    detailpokemon = pokemonAlreadyLoaded[pokemon.name];
+  }else{
+    const detailResponse = await fetch(pokemon.url)
+    detailpokemon = await detailResponse.json();
+    pokemonAlreadyLoaded[pokemon.name] = detailpokemon;
+  }
+  pokemon_list.innerHTML += templateRenderPokemon(detailpokemon);
+  
+}
+
+async function ShowPokemonById(id) {
+  const container = document.getElementById("pokemon_list");
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    if (!response.ok || response.status === 404)
+      throw new Error("Pokémon not found");
+    const detailpokemon = await response.json();
+    detailPokemonId.innerHTML = templateRenderDetailPokemon(detailpokemon);
+  } catch (error) {
+    container.innerHTML = `<p style="color:red;">${error.message}</p>`;
+  } 
+}
+
+
